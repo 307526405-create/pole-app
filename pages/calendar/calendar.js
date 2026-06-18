a84300069d06c6576996a0cf1d3d9eec7977b00a
307526405-create <307526405@qq.com>
2026-06-18 18:08:37 +0800

    赛程中英双语：中文28rpx+英文20rpx小字

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,19 +1,37 @@
 const API = 'http://localhost:8080/api'
 const D_CN = {
-  'Andrea Kimi Antonelli':'安德烈亚·基米·安托内利','Lewis Hamilton':'刘易斯·汉密尔顿','George Russell':'乔治·拉塞尔',
-  'Max Verstappen':'马克斯·维斯塔潘','Lando Norris':'兰多·诺里斯','Oscar Piastri':'奥斯卡·皮亚斯特里',
-  'Charles Leclerc':'夏尔·勒克莱尔','Fernando Alonso':'费尔南多·阿隆索','Lance Stroll':'兰斯·斯特罗尔',
-  'Alexander Albon':'亚历山大·阿尔本','Carlos Sainz':'卡洛斯·赛恩斯','Yuki Tsunoda':'角田裕毅',
-  'Isack Hadjar':'伊萨克·哈贾尔','Pierre Gasly':'皮埃尔·加斯利','Jack Doohan':'杰克·杜汉',
-  'Franco Colapinto':'弗朗哥·科拉平托','Esteban Ocon':'埃斯特班·奥康','Oliver Bearman':'奥利弗·贝尔曼',
-  'Nico Hülkenberg':'尼科·霍肯伯格','Gabriel Bortoleto':'加布里埃尔·博托莱托',
-  'Sergio Pérez':'塞尔吉奥·佩雷兹','Liam Lawson':'利亚姆·劳森','周冠宇':'周冠宇','Guanyu Zhou':'周冠宇',
+  'Andrea Kimi Antonelli':['安德烈亚·基米·安托内利','Kimi Antonelli'],
+  'Lewis Hamilton':['刘易斯·汉密尔顿','Hamilton'],
+  'George Russell':['乔治·拉塞尔','Russell'],
+  'Max Verstappen':['马克斯·维斯塔潘','Verstappen'],
+  'Lando Norris':['兰多·诺里斯','Norris'],
+  'Oscar Piastri':['奥斯卡·皮亚斯特里','Piastri'],
+  'Charles Leclerc':['夏尔·勒克莱尔','Leclerc'],
+  'Fernando Alonso':['费尔南多·阿隆索','Alonso'],
+  'Lance Stroll':['兰斯·斯特罗尔','Stroll'],
+  'Alexander Albon':['亚历山大·阿尔本','Albon'],
+  'Carlos Sainz':['卡洛斯·赛恩斯','Sainz'],
+  'Yuki Tsunoda':['角田裕毅','Tsunoda'],
+  'Isack Hadjar':['伊萨克·哈贾尔','Hadjar'],
+  'Pierre Gasly':['皮埃尔·加斯利','Gasly'],
+  'Jack Doohan':['杰克·杜汉','Doohan'],
+  'Franco Colapinto':['弗朗哥·科拉平托','Colapinto'],
+  'Esteban Ocon':['埃斯特班·奥康','Ocon'],
+  'Oliver Bearman':['奥利弗·贝尔曼','Bearman'],
+  'Nico Hülkenberg':['尼科·霍肯伯格','Hülkenberg'],
+  'Gabriel Bortoleto':['加布里埃尔·博托莱托','Bortoleto'],
+  'Sergio Pérez':['塞尔吉奥·佩雷兹','Pérez'],
+  'Liam Lawson':['利亚姆·劳森','Lawson'],
 }
 const T_CN = {
-  'Mercedes':'梅赛德斯 AMG','Ferrari':'法拉利','McLaren':'迈凯伦','Red Bull':'红牛','Red Bull Racing':'红牛',
-  'Alpine':'阿尔派','Alpine F1 Team':'阿尔派','Aston Martin':'阿斯顿·马丁','Williams':'威廉姆斯',
-  'RB F1 Team':'小红牛','Racing Bulls':'小红牛','Haas':'哈斯','Haas F1 Team':'哈斯',
-  'Sauber':'索伯','Audi':'奥迪','Cadillac F1 Team':'凯迪拉克','Cadillac':'凯迪拉克',
+  'Mercedes':['梅赛德斯 AMG','Mercedes'],'Ferrari':['法拉利','Ferrari'],'McLaren':['迈凯伦','McLaren'],
+  'Red Bull':['红牛','Red Bull'],'Red Bull Racing':['红牛','Red Bull'],
+  'Alpine':['阿尔派','Alpine'],'Alpine F1 Team':['阿尔派','Alpine'],
+  'Aston Martin':['阿斯顿·马丁','Aston Martin'],'Williams':['威廉姆斯','Williams'],
+  'RB F1 Team':['小红牛','RB'],'Racing Bulls':['小红牛','RB'],
+  'Haas':['哈斯','Haas'],'Haas F1 Team':['哈斯','Haas'],
+  'Sauber':['索伯','Sauber'],'Audi':['奥迪','Audi'],
+  'Cadillac F1 Team':['凯迪拉克','Cadillac'],'Cadillac':['凯迪拉克','Cadillac'],
 }
 
 Page({
@@ -30,12 +48,12 @@ Page({
     }})
     wx.request({url:API+'/standings/drivers',success:(res)=>{ 
       const ds=this.data.standings
-      ds[0]=(res.data.data||[]).map(d=>({pos:d.position,name:D_CN[d.driver_name]||d.driver_name,team:T_CN[d.constructor]||d.constructor,pts:d.points,img:''}))
+      ds[0]=(res.data.data||[]).map(d=>{const t=D_CN[d.driver_name]||[d.driver_name,''];return{pos:d.position,name:t[0],en:t[1],team:(T_CN[d.constructor]||[d.constructor])[0],pts:d.points,img:''}})
       this.setData({standings:ds})
     }})
     wx.request({url:API+'/standings/constructors',success:(res)=>{ 
       const ds=this.data.standings
-      ds[1]=(res.data.data||[]).map(d=>({pos:d.position,name:T_CN[d.name]||d.name,pts:d.points,img:''}))
+      ds[1]=(res.data.data||[]).map(d=>{const t=T_CN[d.name]||[d.name,''];return{pos:d.position,name:t[0],en:t[1],pts:d.points,img:''}})
       this.setData({standings:ds})
     }})
   },
