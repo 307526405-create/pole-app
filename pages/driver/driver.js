bdbd6b85da590dd6ee5a28a61f7e1eb0615f9652
307526405-create <307526405@qq.com>
2026-06-19 02:18:08 +0800

    driver页极简内联样式调试

--- pages/driver/driver.js
+++ pages/driver/driver.js
@@ -1,19 +1,10 @@
 Page({
-  data: { driver: null },
+  data: { driver: { num:'?', name:'加载中', team:'', nationality:'', championships:'0', wins:'0', podiums:'0', desc:'' } },
   onLoad() {
-    const app = getApp()
-    const d = app.globalData.driver
-    console.log('driver page loaded:', d)
+    var app = getApp()
+    var d = app.globalData.driver
     if (d && d.name) {
       this.setData({ driver: d })
-    } else {
-      // fallback测试数据
-      this.setData({ driver: {
-        num: 1, name: '马克斯·维斯塔潘', team: '红牛', nationality: '🇳🇱',
-        championships: '4次', wins: '63+', podiums: '112+',
-        desc: '4届世界冠军。2026赛季红牛低迷，暂列第7。',
-        img: 'https://media.formula1.com/image/upload/c_fill,w_400,h_400,g_face/q_auto/v1740000001/common/f1/2026/redbullracing/maxver01/2026redbullracingmaxver01right.webp'
-      }})
     }
   },
   onBack() { wx.navigateBack() }
