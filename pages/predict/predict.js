6a11bc8855590a4db922377def890c07b367a156
307526405-create <307526405@qq.com>
2026-06-19 02:41:19 +0800

    重写predict.js：全fail回调+干净格式

--- pages/predict/predict.js
+++ pages/predict/predict.js
@@ -1,6 +1,5 @@
 const API = 'http://localhost:8080/api'
 
-// 车手永久编号
 const PERM_NUM = {
   'Andrea Kimi Antonelli':12,'Lewis Hamilton':44,'George Russell':63,'Charles Leclerc':16,
   'Lando Norris':1,'Oscar Piastri':81,'Max Verstappen':3,'Pierre Gasly':10,
@@ -11,80 +10,112 @@ const PERM_NUM = {
 }
 
 Page({
-  data: { step:0, showCelebrate:false, canSubmit:false, raceRound:8,
+  data: {
+    step:0, showCelebrate:false, canSubmit:false, raceRound:8,
     stepTitles:['1/6 杆位','2/6 排位赛前三（选3人）','3/6 正赛领奖台（选3人）','4/6 最快圈','5/6 安全车次数（含SC和VSC）','6/6 退赛车数'],
-    drivers:[], options:[], submitData:{pole:'',podium:[],fastest_lap:'',safety_car:'',retirements:''} },
-  
-  onLoad(){
-    var that=this
-    wx.request({url:API+'/standings/drivers',success(res){
-      that.setData({drivers:(res.data.data||[]).map(function(d){return{num:PERM_NUM[d.driver_name]||d.position,name:d.driver_name,team:d.constructor,selected:false}})})
-    }})
-    wx.request({url:API+'/races',success(res){
-      var next=(res.data.data||[]).find(function(r){return new Date(r.date)>new Date()})
-      if(next) that.setData({raceRound:next.round})
-    }})
+    drivers:[], options:[]
   },
 
-  selectDriver(e){
-    var that=this;var idx=e.currentTarget.dataset.idx;var step=this.data.step;var drivers=this.data.drivers.slice()
-    if(step===0||step===3){
-      drivers.forEach(function(d,i){d.selected=(i===idx)})
-      this.setData({drivers:drivers});setTimeout(function(){that.nextStep()},300)
+  onLoad(){
+    var that = this
+    wx.request({
+      url: API + '/standings/drivers',
+      success: function(res) {
+        that.setData({drivers: (res.data.data||[]).map(function(d) {
+          return {num: PERM_NUM[d.driver_name]||d.position, name: d.driver_name, team: d.constructor, selected: false}
+        })})
+      },
+      fail: function() { wx.showToast({title:'加载失败',icon:'none'}) }
+    })
+    wx.request({
+      url: API + '/races',
+      success: function(res) {
+        var next = (res.data.data||[]).find(function(r) { return new Date(r.date) > new Date() })
+        if (next) that.setData({raceRound: next.round})
+      },
+      fail: function() { wx.showToast({title:'加载失败',icon:'none'}) }
+    })
+  },
+
+  selectDriver(e) {
+    var that = this, idx = e.currentTarget.dataset.idx, step = this.data.step
+    var drivers = this.data.drivers.map(function(d) { return Object.assign({}, d) })
+
+    if (step === 0 || step === 3) {
+      drivers.forEach(function(d, i) { d.selected = (i === idx) })
+      this.setData({drivers: drivers})
+      setTimeout(function() { that.nextStep() }, 300)
       return
     }
-    var sel=drivers.filter(function(d){return d.selected}).length
-    if(drivers[idx].selected){drivers[idx].selected=false}
-    else if(sel<3){drivers[idx].selected=true}
-    this.setData({drivers:drivers})
-    if(drivers.filter(function(d){return d.selected}).length===3) setTimeout(function(){that.nextStep()},300)
-  },
-
-  nextStep(){
-    var step=this.data.step+1
-    if(step===5){
-      this.setData({step:step,drivers:this.data.drivers.map(function(d){d.selected=false;return d}),options:[{text:'0次',selected:false},{text:'1次',selected:false},{text:'2次及以上',selected:false}]})
-    } else if(step===6){
-      this.setData({step:step,options:[{text:'0-2辆',selected:false},{text:'3-5辆',selected:false},{text:'6辆以上',selected:false}]})
-    } else {
-      this.setData({step:step,drivers:this.data.drivers.map(function(d){d.selected=false;return d})})
+    var sel = drivers.filter(function(d) { return d.selected }).length
+    if (drivers[idx].selected) {
+      drivers[idx].selected = false
+    } else if (sel < 3) {
+      drivers[idx].selected = true
+    }
+    this.setData({drivers: drivers})
+    if (drivers.filter(function(d) { return d.selected }).length === 3) {
+      setTimeout(function() { that.nextStep() }, 300)
     }
   },
 
-  selectOption(e){
-    var that=this;var idx=e.currentTarget.dataset.idx
-    var options=this.data.options.map(function(o,i){o.selected=(i===idx);return o})
-    this.setData({options:options})
-    setTimeout(function(){
-      if(that.data.step===5){
-        that.setData({step:6,options:[{text:'0-2辆',selected:false},{text:'3-5辆',selected:false},{text:'6辆以上',selected:false}]})
-      } else {
-        that.setData({canSubmit:true})
-      }
-    },300)
+  nextStep() {
+    var step = this.data.step + 1
+    var clean = this.data.drivers.map(function(d) { d.selected = false; return d })
+    if (step === 5) {
+      this.setData({step: step, drivers: clean, options: [
+        {text:'0次',selected:false},{text:'1次',selected:false},{text:'2次及以上',selected:false}
+      ]})
+    } else if (step === 6) {
+      this.setData({step: step, options: [
+        {text:'0-2辆',selected:false},{text:'3-5辆',selected:false},{text:'6辆以上',selected:false}
+      ]})
+    } else {
+      this.setData({step: step, drivers: clean})
+    }
   },
 
-  submit(){
-    var that=this
-    var d=this.data
-    // 收集用户选择
-    var selDrivers=d.drivers.filter(function(dr){return dr.selected})
-    var pole=selDrivers.length>0?selDrivers[0].name:''
-    var podium=selDrivers.slice(0,3).map(function(dr){return dr.name})
-    var fastest=selDrivers.length>0?selDrivers[0].name:''
-    var sc=d.options.find(function(o){return o.selected})
-    var safety_car=sc?sc.text:''
-    var retirements=sc?sc.text:''
-    
-    var data={race_round:d.raceRound,pole:pole,podium:podium,fastest_lap:fastest,safety_car:safety_car,retirements:retirements,wx_openid:'test'}
-    
-    wx.request({url:API+'/predict',method:'POST',data:data,success:function(){
-      that.setData({showCelebrate:true})
-      setTimeout(function(){
-        that.setData({showCelebrate:false,step:0,canSubmit:false,drivers:d.drivers.map(function(dr){dr.selected=false;return dr}),options:[],submitData:{pole:'',podium:[],fastest_lap:'',safety_car:'',retirements:''}})
-        wx.showShareMenu({withShareTicket:true})
-        wx.switchTab({url:'/pages/index/index'})},fail:function(){wx.showToast({title:"提交失败",icon:"none"})}
-      },2000)
-    }})
+  selectOption(e) {
+    var that = this, idx = e.currentTarget.dataset.idx
+    var options = this.data.options.map(function(o, i) { return {text: o.text, selected: i === idx} })
+    this.setData({options: options})
+    setTimeout(function() {
+      if (that.data.step === 5) {
+        that.setData({step: 6, options: [
+          {text:'0-2辆',selected:false},{text:'3-5辆',selected:false},{text:'6辆以上',selected:false}
+        ]})
+      } else {
+        that.setData({canSubmit: true})
+      }
+    }, 300)
+  },
+
+  submit() {
+    var that = this, d = this.data
+    var selDrivers = d.drivers.filter(function(dr) { return dr.selected })
+    var data = {
+      race_round: d.raceRound,
+      pole: selDrivers.length > 0 ? selDrivers[0].name : '',
+      podium: selDrivers.slice(0, 3).map(function(dr) { return dr.name }),
+      fastest_lap: selDrivers.length > 0 ? selDrivers[0].name : '',
+      safety_car: d.options.find(function(o) { return o.selected }) ? d.options.find(function(o) { return o.selected }).text : '',
+      retirements: d.options.find(function(o) { return o.selected }) ? d.options.find(function(o) { return o.selected }).text : '',
+      wx_openid: 'test'
+    }
+    wx.request({
+      url: API + '/predict',
+      method: 'POST',
+      data: data,
+      success: function() {
+        that.setData({showCelebrate: true})
+        setTimeout(function() {
+          that.setData({showCelebrate: false, step: 0, canSubmit: false,
+            drivers: d.drivers.map(function(dr) { dr.selected = false; return dr }),
+            options: []})
+          wx.switchTab({url: '/pages/index/index'})
+        }, 2000)
+      },
+      fail: function() { wx.showToast({title:'提交失败',icon:'none'}) }
+    })
   }
 })
