75c52b6bfa7d7489dd6a462141089ccad2e030a5
307526405-create <307526405@qq.com>
2026-06-18 14:35:26 +0800

    uni-app项目骨架：5个页面路由+TabBar+App.vue

---
+++ App.vue
@@ -0,0 +1,6 @@
+<script>
+export default { onLaunch() { console.log('杆位 App launched') } }
+</script>
+<style>
+page { font-family: "PingFang SC", -apple-system, sans-serif; font-size: 28rpx; color: #333; background: #F5F5F5; }
+</style>
