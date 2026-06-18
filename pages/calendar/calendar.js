373ff08506bbf9a35eff175adbafc9358caf6df0
307526405-create <307526405@qq.com>
2026-06-18 17:12:57 +0800

    车队颜色Logo+车手首字母头像：11支车队专属色

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,5 +1,11 @@
 const API = 'http://localhost:8080/api'
-const { DRIVER_CN, TEAM_CN } = require('../../utils/translate')
+
+const TEAM_COLORS = {
+  'Mercedes':'#00D2BE','Ferrari':'#DC0000','McLaren':'#FF8700','Red Bull':'#3671C6','Red Bull Racing':'#3671C6',
+  'Alpine':'#0090FF','Alpine F1 Team':'#0090FF','Aston Martin':'#006F62','Williams':'#005AFF',
+  'RB F1 Team':'#6692FF','Racing Bulls':'#6692FF','Haas':'#FFFFFF','Haas F1 Team':'#FFFFFF',
+  'Sauber':'#52E252','Audi':'#000000','Cadillac F1 Team':'#C0A060','Cadillac':'#C0A060'
+}
 
 Page({
   onShow() { if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 1 }) },
@@ -15,12 +21,21 @@ Page({
     }})
     wx.request({ url: API + '/standings/drivers', success: (res) => { 
       const ds = this.data.standings
-      ds[0] = (res.data.data||[]).map(d => ({ pos: d.position, name: DRIVER_CN[d.driver_name] || d.driver_name, team: TEAM_CN[d.constructor] || d.constructor, pts: d.points }))
+      ds[0] = (res.data.data||[]).map(d => {
+        const cn = d.driver_name || ''
+        const team = d.constructor || ''
+        const initials = cn.split(' ').map(w => w[0]).join('').substring(0,2)
+        return { pos: d.position, name: cn, team: team, pts: d.points, initials: initials, color: TEAM_COLORS[team] || '#999' }
+      })
       this.setData({ standings: ds })
     }})
     wx.request({ url: API + '/standings/constructors', success: (res) => { 
       const ds = this.data.standings
-      ds[1] = (res.data.data||[]).map(d => ({ pos: d.position, name: TEAM_CN[d.name] || d.name, team: '', pts: d.points }))
+      ds[1] = (res.data.data||[]).map(d => {
+        const cn = d.name || ''
+        const initials = cn.substring(0,2).toUpperCase()
+        return { pos: d.position, name: cn, team: '', pts: d.points, initials: initials, color: TEAM_COLORS[cn] || '#999' }
+      })
       this.setData({ standings: ds })
     }})
   },
