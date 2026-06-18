028171034283a3baca0da51448de5e45b320e652
307526405-create <307526405@qq.com>
2026-06-18 16:46:36 +0800

    jolpica真实数据：21车手+10车队+正确积分排序

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -29,21 +29,22 @@ Page({
       [
         {pos:1,name:'兰多·诺里斯',team:'迈凯伦',pts:423},{pos:2,name:'马克斯·维斯塔潘',team:'红牛',pts:421},
         {pos:3,name:'奥斯卡·皮亚斯特里',team:'迈凯伦',pts:410},{pos:4,name:'乔治·拉塞尔',team:'梅赛德斯',pts:319},
-        {pos:5,name:'夏尔·勒克莱尔',team:'法拉利',pts:242},{pos:6,name:'刘易斯·汉密尔顿',team:'法拉利',pts:223},
-        {pos:7,name:'基米·安东内利',team:'梅赛德斯',pts:148},{pos:8,name:'周冠宇',team:'阿尔派',pts:96},
-        {pos:9,name:'费尔南多·阿隆索',team:'阿斯顿马丁',pts:72},{pos:10,name:'皮埃尔·加斯利',team:'阿尔派',pts:65},
-        {pos:11,name:'塞尔吉奥·佩雷兹',team:'红牛',pts:58},{pos:12,name:'亚历山大·阿尔本',team:'威廉姆斯',pts:46},
-        {pos:13,name:'卡洛斯·赛恩斯',team:'威廉姆斯',pts:42},{pos:14,name:'角田裕毅',team:'小红牛',pts:38},
-        {pos:15,name:'埃斯特班·奥康',team:'哈斯',pts:31},{pos:16,name:'兰斯·斯特罗尔',team:'阿斯顿马丁',pts:22},
-        {pos:17,name:'尼科·霍肯伯格',team:'索伯',pts:18},{pos:18,name:'奥利弗·贝尔曼',team:'哈斯',pts:14},
-        {pos:19,name:'伊萨克·哈贾尔',team:'小红牛',pts:8},{pos:20,name:'加布里埃尔·博托莱托',team:'索伯',pts:4}
+        {pos:5,name:'夏尔·勒克莱尔',team:'法拉利',pts:242},{pos:6,name:'刘易斯·汉密尔顿',team:'法拉利',pts:156},
+        {pos:7,name:'安德烈亚·基米·安东内利',team:'梅赛德斯',pts:150},{pos:8,name:'亚历山大·阿尔本',team:'威廉姆斯',pts:73},
+        {pos:9,name:'卡洛斯·赛恩斯',team:'威廉姆斯',pts:64},{pos:10,name:'费尔南多·阿隆索',team:'阿斯顿马丁',pts:56},
+        {pos:11,name:'尼科·霍肯伯格',team:'索伯',pts:51},{pos:12,name:'伊萨克·哈贾尔',team:'小红牛',pts:51},
+        {pos:13,name:'奥利弗·贝尔曼',team:'哈斯',pts:41},{pos:14,name:'利亚姆·劳森',team:'红牛',pts:38},
+        {pos:15,name:'埃斯特班·奥康',team:'哈斯',pts:38},{pos:16,name:'兰斯·斯特罗尔',team:'阿斯顿马丁',pts:33},
+        {pos:17,name:'角田裕毅',team:'小红牛',pts:33},{pos:18,name:'皮埃尔·加斯利',team:'阿尔派',pts:22},
+        {pos:19,name:'加布里埃尔·博托莱托',team:'索伯',pts:19},{pos:20,name:'弗朗哥·科拉平托',team:'阿尔派',pts:0},
+        {pos:21,name:'杰克·杜汉',team:'阿尔派',pts:0}
       ],
       [
-        {pos:1,name:'迈凯伦 McLaren',pts:833},{pos:2,name:'梅赛德斯 Mercedes',pts:467},
-        {pos:3,name:'法拉利 Ferrari',pts:465},{pos:4,name:'红牛 Red Bull',pts:456},
-        {pos:5,name:'阿尔派 Alpine',pts:161},{pos:6,name:'阿斯顿马丁 Aston Martin',pts:94},
-        {pos:7,name:'威廉姆斯 Williams',pts:88},{pos:8,name:'小红牛 Racing Bulls',pts:46},
-        {pos:9,name:'哈斯 Haas',pts:31},{pos:10,name:'索伯 Sauber',pts:18}
+        {pos:1,name:'迈凯伦 McLaren',pts:833},{pos:2,name:'梅赛德斯 Mercedes',pts:469},
+        {pos:3,name:'红牛 Red Bull',pts:451},{pos:4,name:'法拉利 Ferrari',pts:398},
+        {pos:5,name:'威廉姆斯 Williams',pts:137},{pos:6,name:'小红牛 RB',pts:92},
+        {pos:7,name:'阿斯顿马丁 Aston Martin',pts:89},{pos:8,name:'哈斯 Haas',pts:79},
+        {pos:9,name:'索伯 Sauber',pts:70},{pos:10,name:'阿尔派 Alpine',pts:22}
       ]
     ]
   },
