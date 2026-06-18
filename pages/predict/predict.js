04b50822d3bdb4d775c8e0e90670eb0c9460e9da
307526405-create <307526405@qq.com>
2026-06-19 02:37:24 +0800

    分享卡片Canvas：格子旗底+杆位logo+最高38分+引流文案

--- pages/predict/predict.js
+++ pages/predict/predict.js
@@ -26,7 +26,44 @@ Page({
   selectOption(e) { const idx = e.currentTarget.dataset.idx; const options = this.data.options.map((o,i) => ({...o, selected: i === idx})); this.setData({ options }); setTimeout(() => { if (this.data.step === 5) { this.setData({ step: 6, options: [{text:'0-2辆',selected:false},{text:'3-5辆',selected:false},{text:'6辆以上',selected:false}] }) } else { this.setData({ canSubmit: true }) } }, 300) },
   submit() {
     wx.request({ url: API + '/predict', method: 'POST', data: { race_round: this.data.raceRound, pole: '', podium: [], fastest_lap: '', safety_car: '', retirements: '', wx_openid: 'test' },
-      success: () => { this.setData({ showCelebrate: true }); setTimeout(() => { this.setData({ showCelebrate: false, step: 0, canSubmit: false, drivers: this.data.drivers.map(d=>({...d,selected:false})), options: [] }); wx.switchTab({ url: '/pages/index/index' }) }, 2000);wx.showShareMenu({withShareTicket:true}) }
+      success: () => { this.setData({ showCelebrate: true }); setTimeout(() => { this.setData({ showCelebrate: false, step: 0, canSubmit: false, drivers: this.data.drivers.map(d=>({...d,selected:false})), options: [] }); wx.switchTab({ url: '/pages/index/index' }) 
+      // 画分享卡片
+      var ctx = wx.createCanvasContext('shareCanvas', that)
+      ctx.setFillStyle('#0a0a0a')
+      ctx.fillRect(0, 0, 600, 400)
+      // 格子旗纹理
+      for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 5; j++) {
+          if ((i + j) % 2 === 0) {
+            ctx.setFillStyle('rgba(255,255,255,0.06)')
+            ctx.fillRect(i * 75, j * 80, 75, 80)
+          }
+        }
+      }
+      ctx.setFillStyle('#E10600')
+      ctx.setFontSize(32)
+      ctx.setTextAlign('center')
+      ctx.fillText('杆位 POLE', 300, 80)
+      ctx.setFillStyle('#fff')
+      ctx.setFontSize(26)
+      ctx.fillText('竞猜已提交', 300, 140)
+      ctx.setFillStyle('#52C41A')
+      ctx.setFontSize(56)
+      ctx.fillText('最高可得 38 分', 300, 230)
+      ctx.setFillStyle('rgba(255,255,255,0.4)')
+      ctx.setFontSize(20)
+      ctx.fillText('打开小程序「杆位」一起猜F1', 300, 300)
+      ctx.setFillStyle('rgba(255,255,255,0.2)')
+      ctx.setFontSize(16)
+      ctx.fillText('微信搜索「杆位」', 300, 340)
+      ctx.draw(false, function(){
+        setTimeout(function(){
+          wx.canvasToTempFilePath({canvasId:'shareCanvas',success:function(res){
+            that.setData({shareImg:res.tempFilePath})
+          },fail:function(){}}, that)
+        }, 800)
+      })
+}, 2000);wx.showShareMenu({withShareTicket:true}) }
     })
   }
 })
