065e456f438d45c152b8263e4953401c17b46aa2
307526405-create <307526405@qq.com>
2026-06-18 19:52:38 +0800

    恢复我的页面

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,11 +1,10 @@
 Page({
   data: {
     history: [
-      {race:'西班牙站',detail:'杆位✅ 领奖台2/3 ✅ 最快圈❌',score:'+11'},
-      {race:'摩纳哥站',detail:'杆位❌ 领奖台1/3 最快圈❌',score:'+3'},
-      {race:'迈阿密站',detail:'杆位✅ 领奖台3/3 ✅ 最快圈✅',score:'+16'},
+      {race:'🇪🇸 西班牙站',detail:'杆位✅ 领奖台2/3 ✅ 最快圈❌',score:'+11'},
+      {race:'🇲🇨 摩纳哥站',detail:'杆位❌ 领奖台1/3 最快圈❌',score:'+3'},
+      {race:'🇺🇸 迈阿密站',detail:'杆位✅ 领奖台3/3 ✅ 最快圈✅',score:'+16'},
     ]
   },
-  onShow() { if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 2 }) },
-  onPro() { wx.showToast({ title: 'Pro功能即将上线', icon: 'none' }) }
+  onShow() { if (typeof this.getTabBar === 'function' && this.getTabBar()) this.getTabBar().setData({ selected: 2 }) }
 })
