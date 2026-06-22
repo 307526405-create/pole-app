37f5dcba684337a93fdb85e0d837ece1687a7a2b
307526405-create <307526405@qq.com>
2026-06-22 14:06:44 +0800

    极简版：纯文字+原生选择器

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,54 +1,39 @@
-
 Page({
   data: {
     history: [
       {race:'西班牙站',score:11,items:[{txt:'杆位✅',cls:'hit'},{txt:'领奖台2/3',cls:'hit'},{txt:'最快圈❌',cls:'miss'}]},
       {race:'摩纳哥站',score:3,items:[{txt:'杆位❌',cls:'miss'},{txt:'领奖台1/3',cls:'hit'},{txt:'最快圈❌',cls:'miss'}]},
-      {race:'迈阿密站',score:16,items:[{txt:'杆位✅',cls:'hit'},{txt:'领奖台3/3',cls:'hit'},{txt:'最快圈✅',cls:'hit'}]}
+      {race:'迈阿密站',score:16,items:[{txt:'杆位✅',cls:'hit'},{txt:'领奖台3/3',cls:'hit'},{txt:'最快圈✅',cls:'hit'}]},
     ],
-    favTeamName: '',
-    favDriverName: '',
-    favTeamLogo: '',
-    favDriverImg: '',
-    T: ['Ferrari','McLaren','Mercedes','Red Bull','Alpine','Aston Martin','Williams','RB','Haas','Audi','Cadillac'],
-    D: ['Verstappen','Norris','Leclerc','Hamilton','Piastri','Russell','Antonelli'],
-    CNT: {'Ferrari':'法拉利 Ferrari','McLaren':'迈凯伦 McLaren','Mercedes':'梅赛德斯 Mercedes','Red Bull':'红牛 Red Bull','Alpine':'阿尔派 Alpine','Aston Martin':'阿斯顿马丁 Aston Martin','Williams':'威廉姆斯 Williams','RB':'小红牛 RB','Haas':'哈斯 Haas','Audi':'奥迪 Audi','Cadillac':'凯迪拉克 Cadillac'},
-    CND: {'Verstappen':'维斯塔潘 Verstappen','Norris':'诺里斯 Norris','Leclerc':'勒克莱尔 Leclerc','Hamilton':'汉密尔顿 Hamilton','Piastri':'皮亚斯特里 Piastri','Russell':'拉塞尔 Russell','Antonelli':'安东内利 Antonelli'},
-    TL: {'Ferrari':'https://media.formula1.com/content/dam/fom-website/teams/2025/ferrari-logo.png.transform/2col-retina/image.png','McLaren':'https://media.formula1.com/content/dam/fom-website/teams/2025/mclaren-logo.png.transform/2col-retina/image.png','Mercedes':'https://media.formula1.com/content/dam/fom-website/teams/2025/mercedes-logo.png.transform/2col-retina/image.png','Red Bull':'https://media.formula1.com/content/dam/fom-website/teams/2025/red-bull-racing-logo.png.transform/2col-retina/image.png','Alpine':'https://media.formula1.com/content/dam/fom-website/teams/2025/alpine-logo.png.transform/2col-retina/image.png','Aston Martin':'https://media.formula1.com/content/dam/fom-website/teams/2025/aston-martin-logo.png.transform/2col-retina/image.png','Williams':'https://media.formula1.com/content/dam/fom-website/teams/2025/williams-logo.png.transform/2col-retina/image.png','RB':'https://media.formula1.com/content/dam/fom-website/teams/2025/racing-bulls-logo.png.transform/2col-retina/image.png','Haas':'https://media.formula1.com/content/dam/fom-website/teams/2025/haas-logo.png.transform/2col-retina/image.png','Audi':'/static/logos/audi.svg','Cadillac':'/static/logos/cadillac.svg'},
-    DI: {'Verstappen':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/redbullracing/maxver01/2026redbullracingmaxver01right.webp','Norris':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/mclaren/lannor01/2026mclarenlannor01right.webp','Leclerc':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/ferrari/chalec01/2026ferrarichalec01right.webp','Hamilton':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/ferrari/lewham01/2026ferrarilewham01right.webp','Piastri':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/mclaren/oscpia01/2026mclarenoscpia01right.webp','Russell':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/mercedes/georus01/2026mercedesgeorus01right.webp','Antonelli':'https://media.formula1.com/image/upload/c_fill,w_100,h_100,g_face/q_auto/v1740000001/common/f1/2026/mercedes/andant01/2026mercedesandant01right.webp'},
-
+    favTeam: 'Ferrari',
+    favDriver: 'Leclerc'
   },
   onShow() {
     if (typeof this.getTabBar === 'function') this.getTabBar().setData({ selected: 2 })
-    this.refresh()
-  },
-  refresh() {
-    var ft = wx.getStorageSync('favTeam') || 'Ferrari'
-    var fd = wx.getStorageSync('favDriver') || 'Leclerc'
     this.setData({
-      favTeamName: this.data.this.data.CNT[ft] || ft,
-      favDriverName: this.data.this.data.CND[fd] || fd,
-      favTeamLogo: this.data.this.data.TL[ft] || '',
-      favDriverImg: this.data.this.data.DI[fd] || ''
+      favTeam: wx.getStorageSync('favTeam') || 'Ferrari',
+      favDriver: wx.getStorageSync('favDriver') || 'Leclerc'
     })
   },
   pickTeam() {
     var that = this
     wx.showActionSheet({
-      itemList: this.data.T.map(function(k) { return CNT[k] || k }),
+      itemList: ['法拉利','迈凯伦','梅赛德斯','红牛','阿尔派','阿斯顿马丁','威廉姆斯','小红牛','哈斯','奥迪','凯迪拉克'],
       success: function(res) {
-        wx.setStorageSync('favTeam', this.data.T[res.tapIndex])
-        that.refresh()
+        var teams = ['Ferrari','McLaren','Mercedes','Red Bull','Alpine','Aston Martin','Williams','RB','Haas','Audi','Cadillac']
+        wx.setStorageSync('favTeam', teams[res.tapIndex])
+        that.setData({ favTeam: teams[res.tapIndex] })
       }
     })
   },
   pickDriver() {
     var that = this
     wx.showActionSheet({
-      itemList: this.data.D.map(function(k) { return CND[k] || k }),
+      itemList: ['维斯塔潘','诺里斯','勒克莱尔','汉密尔顿','皮亚斯特里','拉塞尔','安东内利'],
       success: function(res) {
-        wx.setStorageSync('favDriver', this.data.D[res.tapIndex])
-        that.refresh()
+        var drivers = ['Verstappen','Norris','Leclerc','Hamilton','Piastri','Russell','Antonelli']
+        wx.setStorageSync('favDriver', drivers[res.tapIndex])
+        that.setData({ favDriver: drivers[res.tapIndex] })
       }
     })
   },
